b727259ddaad81f8225ae975838b325d40aeee6e
Bashkim Grepi <bashkim.grepi@gmail.com>
2025-09-20 13:50:09 +0300

    New sctructure folder version 1 (authentication), - after authentication completed more folders and files will be added to structure

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const page = () => {
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold text-gray-900 mb-6">Dasboard</h1>
+        <div className="bg-white rounded-lg shadow-sm p-6">
+          <p className="text-gray-600">View your dashboard here.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page;
